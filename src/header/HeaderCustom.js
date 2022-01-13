84c55c8268a1442befea8358850c0472f27c7869
Juan S. Rodriguez M <juan.rodriguez@linktic.co>
2022-01-12 19:04:13 -0500

    gitignore added. Asistencia puestos de votacion, login, header, y login creados segun el Figma, vista para forgotPassword ha sido creada. Themes de Material UI ha sido creado para reusar a lo largo de toda la app.

---
+++ src/header/HeaderCustom.js
@@ -0,0 +1,20 @@
+import react from 'react'
+import {AppBar, Toolbar, IconButton, Box} from '@mui/material'
+import logoRnecXxi from "../images/logos_web_sigloXXI_negro.svg";
+import logoRnec from "../images/registraduria-nacional.svg";
+import {useStyles} from "../theme/themeStyles";
+const Header = () => {
+    const classes = useStyles()
+
+    return (
+        <Box sx={{bgcolor: 'bg'}}>
+            <AppBar position="static">
+                <Toolbar >
+                        <img src={logoRnec} alt='logoRnec' className={classes.logoSmall} />
+                        <img src={logoRnecXxi} alt='logoRnec' className={classes.logo} />
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+export default Header
