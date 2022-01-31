a71b0aab1c70452b86d0159ad28f23dd35a00877
aderiver <aderiver@gmail.com>
2022-01-30 22:18:48 -0500

    + Se añade validaciones y opcion para enviar los datos refs #2372

--- src/components/informeKitElectoral/InformeKitElectoral.js
+++ src/components/informeKitElectoral/InformeKitElectoral.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import react, { useState } from 'react';
 import HeaderCustom from "../header/HeaderCustom";
 import Footer from "../footer/Footer";
 import {Box, Typography, Container, Grid, TextField, FormControl} from "@mui/material";
@@ -9,27 +9,92 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
 import React from "react";
+import axios from "axios";
 
+
+
+import useAuthenticationService from "../../domains/AuthenticationService";
 import checkGreenIcon from '../../images/estados/checkGreenIcon.svg'
 import dangerYellowIcon from '../../images/estados/dangerYellowIcon.svg'
 import CommonButton from "../commons/CommonButton";
 
 
-const InformeKitElectoral = () => {
-    const {control, formState} = useForm()
-    const {errors} = formState
 
-    const mesas = [
-        {
-            name: 'Mesa 1',
-        },
-        {
-            name: 'Mesa 2'
-        },
-        {
-            name: 'Mesa Test 1'
+const InformeKitElectoral =  () => {
+   
+    const {control, formState} = useForm()
+    const {errors} = formState;
+    const [mesas, setMesas] = useState([]);
+    const [count, setCount] = useState({
+        'bueno':0,
+        'dano':0,
+        'inservible':0,
+        'faltantes':0
+
+    });
+    const [formulario, setFormulario] = useState({
+        number_kits:0,
+        mesa:{},
+        estado:null
+    });
+
+    //const authenticationService = useAuthenticationService()
+    //const currentUser = authenticationService.currentUser();
+    //const document = currentUser.identityDocument
+    
+    const document = 1120387794; //@todo todavia  no funciona lo de currentUser
+
+    const url = process.env.API_PUESTOS_URL+'/delegates?document='+document;
+    
+
+    
+    axios.get(url)
+      .then(res => {
+
+        if (!res.data.data[0]['attributes']['tableAssignment']) {
+            return false;
         }
-    ]
+        const mesasLael = [];
+        res.data.data[0]['attributes']['tableAssignment'].forEach( function(element, index) {
+            mesasLael.push({label: 'Mesa '+element,id:element,name:'mesa['+element+']'});
+        });
+
+        setMesas(mesasLael);
+
+    });
+
+
+    const handleSubmit = (event) => {
+        const formData = new FormData(event.currentTarget);
+        event.preventDefault();
+        for (let [key, value] of formData.entries()) {
+          console.log(key, value);
+        }
+    };
+
+
+    const changeTable = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        
+
+        count[value]++;
+
+
+        console.log(name);
+        console.log('=');
+        console.log(value);
+        console.log(count);
+    }
+
+    
+
+    
+
+
+
 /*
     const fields = [
         {
@@ -90,6 +155,7 @@ const InformeKitElectoral = () => {
                         Informe puesto de votación
                     </Typography>
                 </Box>
+                <form onSubmit={handleSubmit}>
                 <Grid container sx={{borderTop: 1, borderBottom: 1, borderColor: "grey.grisDetalles", py: "15px", justifyContent: 'space-between'  }} >
                     <Grid item xs={6} sx={{my: "auto"}} >
                         <Typography variant="titleInput">Numero de kits a recibir</Typography>
@@ -98,12 +164,14 @@ const InformeKitElectoral = () => {
                         <TextField
                             type="number"
                             variant="outlined"
+                            name ="numer_kits"
                             required={true}
                             autoFocus={true}
                         />
                     </Grid>
                 </Grid>
-                <Grid container sx={{mt: 6, borderTop: 1, borderColor: "grey.grisDetalles", py: "15px" }} >
+
+                 <Grid container sx={{mt: 6, borderTop: 1, borderColor: "grey.grisDetalles", py: "15px" }} >
                     <Grid item xs={12} sx={{my: "auto", ml: 3}} >
                         <Typography variant="titleInput">Seleccione el Estado</Typography>
                     </Grid>
@@ -114,18 +182,18 @@ const InformeKitElectoral = () => {
                                     <FormControl component="fieldset" sx={{width: 1, display: 'flex', flexDirection: 'row'  }} >
                                         <FormLabel sx={{width: 0.4}} >
                                             <Typography variant="h3" sx={{mb: 3, mt: 3, color: 'grey.grisOscuro'}}>
-                                                {mesa.name}
+                                                {mesa.label}
                                             </Typography>
                                         </FormLabel>
                                         <RadioGroup
                                             //error={error}
-                                            name='mesasEstado'
+                                            name={mesa.name}
                                             //value={value }
                                             //onChange={onChange}
                                             sx={{width: 1, display: 'flex', flexDirection: 'row', justifyItems: 'space-around'}}
                                         >
                                             {estados.map(estado => (
-                                                <Radio control={control} value={estado.value} key={estado.name} sx={{
+                                                <Radio  onChange={changeTable} control={control} value={estado.value} key={estado.name} sx={{
                                                     width: '55px',
                                                     '&, &.Mui-checked': {
                                                         color: estado.color,
@@ -140,6 +208,7 @@ const InformeKitElectoral = () => {
                         })
                     }
                 </Grid>
+               
                 {
                     estados.map(estado => (
                         <Grid container sx={{ borderBottom: 1, borderColor: "grey.grisDetalles", py: "15px", justifyContent: 'space-between' }} >
@@ -152,15 +221,17 @@ const InformeKitElectoral = () => {
                                     variant="outlined"
                                     required={true}
                                     autoFocus={true}
-                                    value={estado.value}
+                                    name={estado.value}
+                                    value={count[estado.value]}
                                 />
                             </Grid>
                         </Grid>
                     ))
                 }
                 <Grid container columns={1} direction="column" alignItems='center' >
-                    <CommonButton type="primario" text="GUARDAR" href="informes_puestos_votacion2" sx={{mb: 10, mt: 20}}/>
+                    <button type="submit">Guardar</button>
                 </Grid>
+                </form>
                 <Footer/>
             </Container>
         </>
